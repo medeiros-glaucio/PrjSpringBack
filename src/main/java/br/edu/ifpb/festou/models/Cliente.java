8d5795362f56d76e2ccf645c39015a071c467fb7
medeiros-glaucio <114786641+medeiros-glaucio@users.noreply.github.com>
2022-12-16 16:05:30 -0300

    Add files via upload

---
+++ src/main/java/br/edu/ifpb/festou/models/Cliente.java
@@ -0,0 +1,54 @@
+package br.edu.ifpb.festou.models;
+
+import jakarta.persistence.*;
+import org.apache.catalina.User;
+import org.w3c.dom.Text;
+
+import java.util.List;
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private int idade;
+    @Column(unique = true)
+    private String nome;
+    @Column
+    private String telefone;
+    @Column
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
